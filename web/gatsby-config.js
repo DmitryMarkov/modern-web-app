4f5dc2629470bd0323dabe48d9080cdfdf438aed
DmitryMarkov <forall@stylive.com>
2019-12-16 22:16:17 +0100

    feat: added rebass and theming

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Modern web app`,
+    description: `Build a web app with modern stack setup`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-theme-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
